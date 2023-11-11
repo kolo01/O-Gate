a1964020122ecde47c4d3368d643e5a768593aee
kolo01 <konedieu5@gmail.com>
2023-11-11 18:17:08 +0100

    thank you

--- pages/notifications.js
+++ pages/notifications.js
@@ -9,24 +9,23 @@ export default function Offers(){
         <Box width={"788px"} pt={10} boxShadow={"rgba(0, 0, 0, 0.35) 0px 5px 15px"} height={"814px"} mt={"96px"}   borderRadius={"25px"} bgColor={"white"}>
         <Flex ml={10} mb={2} border={"1px solid #F1F1F1"} w={"fit-content"} p={2}>
                 <Image src="./sort.png" alt="sort" />
-                <Text color={"#CDCDCD"} fontSize={"16px"} fontWeight={700}>Trier par</Text>
+                <Text color={"#CDCDCD"} fontSize={"16px"} fontWeight={700}>Filtre</Text>
             </Flex>
             <Box width={"full"} height={"1px"} bgColor={"#CDCDCD"}/>
-            <Text ml={5} color={"#D9D9D9"} mb={5} fontWeight={700}>Enregistré le 12/10/2023</Text>
+            <Text ml={5} color={"#D9D9D9"} mb={5} fontWeight={700}>Il y a 2 jours</Text>
             <Flex ml={10} justifyContent={"space-between"} width={"90%"}>
                 <Box ml={2}>
                     
                     <Flex>
                         <Avatar w={"42px"} h={"42px"} mr={2}/>
                         <Box>
-                            <Text fontWeight={700}>Onomo franck</Text>
-                            <Text fontWeight={700} color={"#D9D9D9"} fontSize={"15px"}>Posté il y 2 jours</Text>
+                        <Text w={"400px"} ml={5} mb={5}>{texted}</Text>
                         </Box>
                     </Flex>
                 </Box>
-                <Button border={"1px solid black"} width={"400px"} bgColor={"transparent"} w={"fit-content"} p={5} _hover={{bgColor:"transparent"}}>Voir plus</Button>
+                <Text ></Text>
             </Flex>
-            <Text w={"400px"} ml={5} mb={5}>{texted}</Text>
+            
             <Box width={"full"} height={"1px"} bgColor={"#CDCDCD"}/>
         </Box>
         </Center>
--- pages/relation.js
+++ pages/relation.js
@@ -1,5 +1,54 @@
+import Navbar from "@/components/home/Navbar";
+import Suggestion from "@/components/src/AfterCo/suggestion";
+import { Search2Icon } from "@chakra-ui/icons";
+import { Avatar, Box, Button, Center, Flex, Image, Input, InputGroup, InputRightElement, Text } from "@chakra-ui/react";
+
 export default function Relation(){
-    return(<>
+    const texted="Térrain de 500m2 disponible à cocody angré Contactez moi en inbox pour plus d’informations"
+    return(<Box bgColor={"#F6F6F6"} pb={10}>
+    <Navbar/>
     
-    </>)
+    <Center display={"flex"}>
+        <Flex>
+        <Box width={"788px"} pt={5} boxShadow={"rgba(0, 0, 0, 0.35) 0px 5px 15px"} height={"814px"}  mt={10}  borderRadius={"25px"} bgColor={"white"}>
+            <Flex ml={10} mb={5} >
+                <Flex  border={"1px solid #F1F1F1"} w={"fit-content"} p={2} mr={20}>
+                <Image src="./sort.png" alt="sort" />
+                <Text color={"#CDCDCD"} fontSize={"16px"} fontWeight={700}>Trier par</Text>
+                </Flex>
+               
+                    <InputGroup width={"335px"} height={"34px"}>
+                    <InputRightElement>
+                    <Search2Icon/>  
+                    </InputRightElement> 
+                    <Input type="text" />
+                    </InputGroup>
+               
+            </Flex>
+            <Box width={"full"} height={"1px"} bgColor={"#CDCDCD"}/>
+ 
+            <Flex ml={10} justifyContent={"space-between"} width={"90%"} my={5}>
+                <Box ml={2}>
+                    
+                    <Flex>
+                        <Avatar w={"85px"} h={"85px"} mr={2}/>
+                        
+                            <Text mt={5} fontWeight={700} fontSize={"25px"}>Onomo franck</Text>
+                           
+                        
+                    </Flex>
+                </Box>
+                <Button mt={5} border={"1px solid #219EF9"} color={"#219EF9"} width={"400px"} bgColor={"transparent"} w={"fit-content"} p={5} _hover={{bgColor:"transparent"}}>Message</Button>
+            </Flex>
+          
+            <Box width={"full"} height={"1px"} bgColor={"#CDCDCD"}/>
+        </Box>
+        <Box mt={10} ml={5}>
+        <Suggestion/>
+        </Box>
+        </Flex>
+        </Center>
+      
+        
+    </Box>)
 }
